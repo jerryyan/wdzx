7bf15c49515ddb4656c4c335da7388bd4d91153e
Administrator <Administrator@2012-20141011VK>
2014-11-27 14:26:38 +0800

    排版

--- admin_edit.php
+++ admin_edit.php
@@ -1,109 +1,108 @@
-<?php include 'common/header.php';?>
-<?php 
+<?php include 'common/header.php'; ?>
+<?php
 $id = $_REQUEST["id"];
 $pt = db_fetch_array("SELECT * FROM wdzx_navigation_links where id=$id;", $conn);
 
-$name 		= $_REQUEST["name"];
-$url 		= $_REQUEST["url"];
-$initial 	= $_REQUEST["initial"];
-$level 		= $_REQUEST["level"];
-$province 	= $_REQUEST["province"];
-$is_hot 	= $_REQUEST["is_hot"];
-if($name != '' && $url != '' && $initial != '' && $level != '' && $province != ''){
-	mysql_query("update wdzx_navigation_links set name='$name',url='$url',initial='$initial',level=$level,province='$province',is_hot=$is_hot where id=$id", $conn);
-	echo "<script>alert('修改成功');window.location.href='/admin_list.php';</script>";	
+$name = $_REQUEST["name"];
+$url = $_REQUEST["url"];
+$initial = $_REQUEST["initial"];
+$level = $_REQUEST["level"];
+$province = $_REQUEST["province"];
+$is_hot = $_REQUEST["is_hot"];
+if ($name != '' && $url != '' && $initial != '' && $level != '' && $province != '') {
+    mysql_query("update wdzx_navigation_links set name='$name',url='$url',initial='$initial',level=$level,province='$province',is_hot=$is_hot where id=$id", $conn);
+    echo "<script>alert('修改成功');window.location.href='/admin_list.php';</script>";
 }
-
 ?>
 <div class="subline"></div>
 <div class="w1000" style="margin-top: 60px; padding-bottom: 70px">
-<div class="left subtable">
-<form action="admin_edit.php" method="post">
-<input type="hidden" id="id" name="id" value="<?php echo $id;?>" />
-<table width="510" border="0" cellspacing="0" cellpadding="0">
+    <div class="left subtable">
+        <form action="admin_edit.php" method="post">
+            <input type="hidden" id="id" name="id" value="<?php echo $id; ?>" />
+            <table width="510" border="0" cellspacing="0" cellpadding="0">
 
-	<tr>
-		<td width="109" class="tbtitle"><span>*</span>网站名称：</td>
-		<td width="401"><input name="name" id="name" type="text" class="subinput" value="<?php echo $pt['name'];?>" /></td>
-	</tr>
-	<tr>
-		<td></td>
-		<td class="lr">如：网贷中心</td>
-	</tr>
-	
-	<tr>
-		<td width="109" class="tbtitle"><span>*</span>URL：</td>
-		<td width="401"><input name="url" id="url" type="text" class="subinput" value="<?php echo $pt['url'];?>" /></td>
-	</tr>
-	<tr>
-		<td></td>
-		<td class="lr">如：网贷中心</td>
-	</tr>
-	
-	
-	<tr>
-		<td width="109" class="tbtitle"><span>*</span>首字母：</td>
-		<td width="401"><input name="initial" id="initial" type="text" class="subinput" value="<?php echo $pt['initial'];?>" /></td>
-	</tr>
-	<tr>
-		<td></td>
-		<td class="lr">如：网贷中心</td>
-	</tr>
-	
-	<tr>
-		<td width="109" class="tbtitle"><span>*</span>是否推荐：</td>
-		<td width="401">
-		<select id="is_hot" name="is_hot">
-			<option value="0">不推荐</option>
-			<option value="1">推荐</option>
-		</select>
-		</td>
-	</tr>
-	<tr>
-		<td></td>
-		<td class="lr">如：网贷中心</td>
-	</tr>
-	
-	<tr>
-		<td width="109" class="tbtitle"><span>*</span>级别：</td>
-		<td width="401">
-		<select id="level" name="level">
-			<option value="1">活跃平台</option>
-			<option value="2">人气平台</option>
-			<option value="3">成长平台</option>
-			<option value="4">新平台</option>
-			<option value="5">风险平台</option>
-		</select>
-		</td>
-	</tr>
-	<tr>
-		<td></td>
-		<td class="lr">如：网贷中心</td>
-	</tr>
-	
-	<tr>
-		<td width="109" class="tbtitle"><span>*</span>省份：</td>
-		<td width="401"><input name="province" id="province" type="text" class="subinput" value="<?php echo $pt['province'];?>" /></td>
-	</tr>
-	<tr>
-		<td></td>
-		<td class="lr">如：网贷中心</td>
-	</tr>
-	
-	
-	<tr>
-		<td>&nbsp;</td>
-		<td><input type="submit" class="subbtn" value="提交申请" /><input
-			name="" type="reset" class="subbtn" value="重新输入" /></td>
-	</tr>
-</table>
-</form>
+                <tr>
+                    <td width="109" class="tbtitle"><span>*</span>网站名称：</td>
+                    <td width="401"><input name="name" id="name" type="text" class="subinput" value="<?php echo $pt['name']; ?>" /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="lr">如：网贷中心</td>
+                </tr>
+
+                <tr>
+                    <td width="109" class="tbtitle"><span>*</span>URL：</td>
+                    <td width="401"><input name="url" id="url" type="text" class="subinput" value="<?php echo $pt['url']; ?>" /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="lr">如：网贷中心</td>
+                </tr>
+
+
+                <tr>
+                    <td width="109" class="tbtitle"><span>*</span>首字母：</td>
+                    <td width="401"><input name="initial" id="initial" type="text" class="subinput" value="<?php echo $pt['initial']; ?>" /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="lr">如：网贷中心</td>
+                </tr>
+
+                <tr>
+                    <td width="109" class="tbtitle"><span>*</span>是否推荐：</td>
+                    <td width="401">
+                        <select id="is_hot" name="is_hot">
+                            <option value="0">不推荐</option>
+                            <option value="1">推荐</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="lr">如：网贷中心</td>
+                </tr>
+
+                <tr>
+                    <td width="109" class="tbtitle"><span>*</span>级别：</td>
+                    <td width="401">
+                        <select id="level" name="level">
+                            <option value="1">活跃平台</option>
+                            <option value="2">人气平台</option>
+                            <option value="3">成长平台</option>
+                            <option value="4">新平台</option>
+                            <option value="5">风险平台</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="lr">如：网贷中心</td>
+                </tr>
+
+                <tr>
+                    <td width="109" class="tbtitle"><span>*</span>省份：</td>
+                    <td width="401"><input name="province" id="province" type="text" class="subinput" value="<?php echo $pt['province']; ?>" /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="lr">如：网贷中心</td>
+                </tr>
+
+
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><input type="submit" class="subbtn" value="提交申请" /><input
+                            name="" type="reset" class="subbtn" value="重新输入" /></td>
+                </tr>
+            </table>
+        </form>
+    </div>
+    <div class="left zs">1.带有 <span>*</span> 的为必填项，请认真填写。<br />
+        2.网站简介和logo地址为选填项，但需注意，只有填 写了网站简介才有机会申请图文链接；只有填写了
+        logo地址才有机会申请图片链接，但最终的链接类型 还取决于本站的设置。<br />
+        3.申请链接请先在贵站的首页做好本站的友情链接。<br />
+        4.本站只与PR>=2的网站交换链接，不符合条件者请 勿提交</div>
+    <div class="clear"></div>
 </div>
-<div class="left zs">1.带有 <span>*</span> 的为必填项，请认真填写。<br />
-2.网站简介和logo地址为选填项，但需注意，只有填 写了网站简介才有机会申请图文链接；只有填写了
-logo地址才有机会申请图片链接，但最终的链接类型 还取决于本站的设置。<br />
-3.申请链接请先在贵站的首页做好本站的友情链接。<br />
-4.本站只与PR>=2的网站交换链接，不符合条件者请 勿提交</div>
-<div class="clear"></div>
-</div>
-<?php include 'common/footer.php';?>
+<?php include 'common/footer.php'; ?>
--- admin_list.php
+++ admin_list.php
@@ -1,118 +1,134 @@
-<?php include 'common/header.php';
+<?php
+include 'common/header.php';
 $province = $_REQUEST["province"];
-$initial  = $_REQUEST["initial"];
+$initial = $_REQUEST["initial"];
 
-$szm = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+$szm = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
 $sql = "select * from wdzx_navigation_links  where 1=1 ";
-if($province != '')
-	$sql .= " and province='$province'";
-if($initial != '')
-	$sql .= " and initial='$initial'";
+if ($province != '')
+    $sql .= " and province='$province'";
+if ($initial != '')
+    $sql .= " and initial='$initial'";
 $dh_list = db_fetch_arrays($sql, $conn);
 ?>
 <div class="tt2">
-  <div class="column">
-  	<?php $province_list = db_fetch_arrays("SELECT province,COUNT(*) AS total FROM wdzx_navigation_links GROUP BY province ORDER BY total DESC;", $conn);?>
-    <div class="left cllist"><span>地区分类：</span>&nbsp;
-    <a href="?initial=<?php echo $initial;?>" <?php if($province == ""){?>class="current"<?php }?>>全部</a>
-    <?php foreach ($province_list as $value) {?>
-    <a href="?province=<?php echo $value["province"];?>&initial=<?php echo $initial;?>" <?php if($province == $value["province"]){?>class="current"<?php }?>><?php echo $value["province"];?>(<?php echo $value["total"];?>)</a>
-    <?php }?>
+    <div class="column">
+<?php $province_list = db_fetch_arrays("SELECT province,COUNT(*) AS total FROM wdzx_navigation_links GROUP BY province ORDER BY total DESC;", $conn); ?>
+        <div class="left cllist"><span>地区分类：</span>&nbsp;
+            <a href="?initial=<?php echo $initial; ?>" <?php if ($province == "") { ?>class="current"<?php } ?>>全部</a>
+            <?php foreach ($province_list as $value) { ?>
+                <a href="?province=<?php echo $value["province"]; ?>&initial=<?php echo $initial; ?>" <?php if ($province == $value["province"]) { ?>class="current"<?php } ?>><?php echo $value["province"]; ?>(<?php echo $value["total"]; ?>)</a>
+<?php } ?>
+        </div>
+        <div class="clear"></div>
+
+        <div class="clline"></div>
+        <div class="cllist2"><span>字母查找：</span>&nbsp;
+            <a href="?province=<?php echo $province; ?>" <?php if ($initial == '') { ?>class="current"<?php } ?>>全</a>
+
+            <?php foreach ($szm as $s) { ?>
+                <a href="?initial=<?php echo $s; ?>&province=<?php echo $province; ?>" <?php if ($initial == $s) { ?>class="current"<?php } ?>><?php echo $s; ?></a>
+<?php } ?>
+            <div class="cler"></div>
+
+            <div class="clear"></div>
+        </div>
+        <a href="/admin_add.php">加入导航</a>
     </div>
-    <div class="clear"></div>
-    
-    <div class="clline"></div>
-    <div class="cllist2"><span>字母查找：</span>&nbsp;
-    <a href="?province=<?php echo $province;?>" <?php if($initial == ''){?>class="current"<?php }?>>全</a>
-    
-    <?php foreach($szm as $s){?>
-    <a href="?initial=<?php echo $s;?>&province=<?php echo $province;?>" <?php if($initial == $s){?>class="current"<?php }?>><?php echo $s;?></a>
-    <?php }?>
-      <div class="cler"></div>
-      
-      <div class="clear"></div>
+    <div class="content">
+        <ul class="left tbalist">
+            <li onmousemove="level_show(0)"><a href="#" class="current" id="type_0">所有平台</a></li>
+            <li onmousemove="level_show(1)"><a href="#" id="type_1">活跃平台</a></li>
+            <li onmousemove="level_show(2)"><a href="#" id="type_2">人气平台</a></li>
+            <li onmousemove="level_show(3)"><a href="#" id="type_3">成长平台</a></li>
+            <li onmousemove="level_show(4)"><a href="#" id="type_4">新平台</a></li>
+            <li onmousemove="level_show(5)"><a href="#" id="type_5">风险平台</a></li>
+        </ul>
+        <div class="left conlist" id="level_0">
+            <div class="clear"></div>
+            <div class="clline"></div>
+            <ul class="conlisttext">
+                <?php foreach ($dh_list as $dh) { ?>
+                    <li><a href="/admin_edit.php?id=<?php echo $dh['id']; ?>" target="_blank"><font size="3px;"><?php echo $dh['name']; ?></font></a></li>
+<?php } ?>
+            </ul>
+            <div class="clear"></div>
+        </div>
+
+        <div class="left conlist" id="level_1" style="display:none;">
+            <ul class="conlisttext">
+                <?php foreach ($dh_list as $dh) {
+                    if ($dh["level"] != 1) {
+                        continue;
+                    } ?>
+                    <li><a href="/admin_edit.php?id=<?php echo $dh['id']; ?>" target="_blank"><font size="3px;"><?php echo $dh['name']; ?></font></a></li>
+<?php } ?>
+            </ul>
+            <div class="clear"></div>
+        </div>
+
+        <div class="left conlist" id="level_2" style="display:none;">
+            <ul class="conlisttext">
+<?php foreach ($dh_list as $dh) {
+    if ($dh["level"] != 2) {
+        continue;
+    } ?>
+                    <li><a href="/admin_edit.php?id=<?php echo $dh['id']; ?>" target="_blank"><font size="3px;"><?php echo $dh['name']; ?></font></a></li>
+                <?php } ?>
+            </ul>
+            <div class="clear"></div>
+        </div>
+
+        <div class="left conlist" id="level_3" style="display:none;">
+            <ul class="conlisttext">
+<?php foreach ($dh_list as $dh) {
+    if ($dh["level"] != 3) {
+        continue;
+    } ?>
+                    <li><a href="/admin_edit.php?id=<?php echo $dh['id']; ?>" target="_blank"><font size="3px;"><?php echo $dh['name']; ?></font></a></li>
+<?php } ?>
+            </ul>
+            <div class="clear"></div>
+        </div>
+
+        <div class="left conlist" id="level_4" style="display:none;">
+            <ul class="conlisttext">
+                <?php foreach ($dh_list as $dh) {
+                    if ($dh["level"] != 4) {
+                        continue;
+                    } ?>
+                    <li><a href="/admin_edit.php?id=<?php echo $dh['id']; ?>" target="_blank"><font size="3px;"><?php echo $dh['name']; ?></font></a></li>
+<?php } ?>
+            </ul>
+            <div class="clear"></div>
+        </div>
+
+        <div class="left conlist" id="level_5" style="display:none;">
+            <ul class="conlisttext">
+<?php foreach ($dh_list as $dh) {
+    if ($dh["level"] != 5) {
+        continue;
+    } ?>
+                    <li><a href="/admin_edit.php?id=<?php echo $dh['id']; ?>" target="_blank"><font size="3px;"><?php echo $dh['name']; ?></font></a></li>
+<?php } ?>
+            </ul>
+            <div class="clear"></div>
+        </div>
+
+        <div class="clear"></div>
     </div>
-    <a href="/admin_add.php">加入导航</a>
-  </div>
-  <div class="content">
-    <ul class="left tbalist">
-      <li onmousemove="level_show(0)"><a href="#" class="current" id="type_0">所有平台</a></li>
-      <li onmousemove="level_show(1)"><a href="#" id="type_1">活跃平台</a></li>
-      <li onmousemove="level_show(2)"><a href="#" id="type_2">人气平台</a></li>
-      <li onmousemove="level_show(3)"><a href="#" id="type_3">成长平台</a></li>
-      <li onmousemove="level_show(4)"><a href="#" id="type_4">新平台</a></li>
-      <li onmousemove="level_show(5)"><a href="#" id="type_5">风险平台</a></li>
-    </ul>
-    <div class="left conlist" id="level_0">
-      <div class="clear"></div>
-      <div class="clline"></div>
-      <ul class="conlisttext">
-        <?php foreach ($dh_list as $dh){?>
-        <li><a href="/admin_edit.php?id=<?php echo $dh['id'];?>" target="_blank"><font size="3px;"><?php echo $dh['name'];?></font></a></li>
-        <?php }?>
-      </ul>
-      <div class="clear"></div>
-    </div>
-    
-    <div class="left conlist" id="level_1" style="display:none;">
-      <ul class="conlisttext">
-        <?php foreach ($dh_list as $dh){if($dh["level"] != 1){continue;}?>
-        <li><a href="/admin_edit.php?id=<?php echo $dh['id'];?>" target="_blank"><font size="3px;"><?php echo $dh['name'];?></font></a></li>
-        <?php }?>
-      </ul>
-      <div class="clear"></div>
-    </div>
-    
-    <div class="left conlist" id="level_2" style="display:none;">
-      <ul class="conlisttext">
-        <?php foreach ($dh_list as $dh){if($dh["level"] != 2){continue;}?>
-        <li><a href="/admin_edit.php?id=<?php echo $dh['id'];?>" target="_blank"><font size="3px;"><?php echo $dh['name'];?></font></a></li>
-        <?php }?>
-      </ul>
-      <div class="clear"></div>
-    </div>
-    
-    <div class="left conlist" id="level_3" style="display:none;">
-      <ul class="conlisttext">
-        <?php foreach ($dh_list as $dh){if($dh["level"] != 3){continue;}?>
-        <li><a href="/admin_edit.php?id=<?php echo $dh['id'];?>" target="_blank"><font size="3px;"><?php echo $dh['name'];?></font></a></li>
-        <?php }?>
-      </ul>
-      <div class="clear"></div>
-    </div>
-    
-    <div class="left conlist" id="level_4" style="display:none;">
-      <ul class="conlisttext">
-        <?php foreach ($dh_list as $dh){if($dh["level"] != 4){continue;}?>
-        <li><a href="/admin_edit.php?id=<?php echo $dh['id'];?>" target="_blank"><font size="3px;"><?php echo $dh['name'];?></font></a></li>
-        <?php }?>
-      </ul>
-      <div class="clear"></div>
-    </div>
-    
-    <div class="left conlist" id="level_5" style="display:none;">
-      <ul class="conlisttext">
-        <?php foreach ($dh_list as $dh){if($dh["level"] != 5){continue;}?>
-        <li><a href="/admin_edit.php?id=<?php echo $dh['id'];?>" target="_blank"><font size="3px;"><?php echo $dh['name'];?></font></a></li>
-        <?php }?>
-      </ul>
-      <div class="clear"></div>
-    </div>
-    
-    <div class="clear"></div>
-  </div>
 </div>
 <script type="text/javascript">
 <!--
-function level_show(id){
-	for(var i = 0; i<6; i++){
-		$("#level_"+i).hide();
-		$("#type_"+i).attr("class", "");
-	}
-	
-	$("#level_"+id).show();
-	$("#type_"+id).attr("class","current");
-}
+    function level_show(id) {
+        for (var i = 0; i < 6; i++) {
+            $("#level_" + i).hide();
+            $("#type_" + i).attr("class", "");
+        }
+
+        $("#level_" + id).show();
+        $("#type_" + id).attr("class", "current");
+    }
 //-->
 </script>
-<?php include 'common/footer.php';?>
+<?php include 'common/footer.php'; ?>
